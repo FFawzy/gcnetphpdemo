db99a9b4341e59d40dc1f68ec52f71b162f0f27a
Fady Fawzy <coofady@gmail.com>
2017-04-09 07:43:57 -0500

    changing the title for the application

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				<h1 id = "message"><?php echo "Hello GCNet!"; ?></h1>
+				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application using Bluemix</span>.
 			</td>
 		</tr>
 	</table>
